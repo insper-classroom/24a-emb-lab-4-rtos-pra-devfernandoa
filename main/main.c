4fc57ad4f5467adde47564d6f1cbe1219842d0dc
devfernandoa <fefealzueta@gmail.com>
2024-03-11 19:56:08 +0000

    Com certeza vai dar mil erros agora

--- main/main.c
+++ main/main.c
@@ -1,13 +1,21 @@
 /*
  * LED blink with FreeRTOS
  */
-#include <FreeRTOS.h>
-#include <task.h>
-#include <semphr.h>
-#include <queue.h>
 
-#include "ssd1306.h"
+#include "hardware/rtc.h"
+#include "hardware/timer.h"
+#include "hardware/uart.h"
+#include "pico/stdlib.h"
+#include "pico/util/datetime.h"
+#include <FreeRTOS.h>
+#include <hardware/gpio.h>
+#include <queue.h>
+#include <semphr.h>
+#include <task.h>
+#include <time.h>
+
 #include "gfx.h"
+#include "ssd1306.h"
 
 #include "pico/stdlib.h"
 #include <stdio.h>
@@ -19,6 +27,23 @@ const uint BTN_3_OLED = 27;
 const uint LED_1_OLED = 20;
 const uint LED_2_OLED = 21;
 const uint LED_3_OLED = 22;
+
+#define TRIG_PIN 12
+#define ECHO_PIN 13
+
+SemaphoreHandle_t xSemaphore = NULL;
+SemaphoreHandle_t xSemaphoreTimeDiff = NULL;
+uint32_t time_start, time_end;
+
+uint32_t time_diff; // Declarar time_diff como uma variável global
+
+bool alarm_fired = false;
+
+int64_t alarm_callback(alarm_id_t id, void *user_data) {
+    alarm_fired = true;
+
+    return 0;
+}
 
 void oled1_btn_led_init(void) {
     gpio_init(LED_1_OLED);
@@ -43,58 +68,57 @@ void oled1_btn_led_init(void) {
     gpio_pull_up(BTN_3_OLED);
 }
 
-void oled1_demo_1(void *p) {
-    printf("Inicializando Driver\n");
-    ssd1306_init();
+void pin_callback(uint gpio, uint32_t events) {
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    uint32_t time = to_us_since_boot(get_absolute_time());
+    if (gpio_get(ECHO_PIN)) {
+        time_start = time;
+    } else {
+        time_end = time;
+        xSemaphoreGiveFromISR(xSemaphore, &xHigherPriorityTaskWoken);
+    }
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+}
 
-    printf("Inicializando GLX\n");
-    ssd1306_t disp;
-    gfx_init(&disp, 128, 32);
-
-    printf("Inicializando btn and LEDs\n");
-    oled1_btn_led_init();
-
-    char cnt = 15;
+void trigger_task(void *pvParameters) {
     while (1) {
+        gpio_put(TRIG_PIN, 1);
+        sleep_us(10);
+        gpio_put(TRIG_PIN, 0);
 
-        if (gpio_get(BTN_1_OLED) == 0) {
-            cnt = 15;
-            gpio_put(LED_1_OLED, 0);
-            gfx_clear_buffer(&disp);
-            gfx_draw_string(&disp, 0, 0, 1, "LED 1 - ON");
-            gfx_show(&disp);
-        } else if (gpio_get(BTN_2_OLED) == 0) {
-            cnt = 15;
-            gpio_put(LED_2_OLED, 0);
-            gfx_clear_buffer(&disp);
-            gfx_draw_string(&disp, 0, 0, 1, "LED 2 - ON");
-            gfx_show(&disp);
-        } else if (gpio_get(BTN_3_OLED) == 0) {
-            cnt = 15;
-            gpio_put(LED_3_OLED, 0);
-            gfx_clear_buffer(&disp);
-            gfx_draw_string(&disp, 0, 0, 1, "LED 3 - ON");
-            gfx_show(&disp);
-        } else {
+        // Define um alarme para daqui a 300 milissegundos
+        alarm_fired = false;
+        add_alarm_in_ms(300, alarm_callback, NULL, false);
 
-            gpio_put(LED_1_OLED, 1);
-            gpio_put(LED_2_OLED, 1);
-            gpio_put(LED_3_OLED, 1);
-            gfx_clear_buffer(&disp);
-            gfx_draw_string(&disp, 0, 0, 1, "PRESSIONE ALGUM");
-            gfx_draw_string(&disp, 0, 10, 1, "BOTAO");
-            gfx_draw_line(&disp, 15, 27, cnt,
-                          27);
-            vTaskDelay(pdMS_TO_TICKS(50));
-            if (++cnt == 112)
-                cnt = 15;
-
-            gfx_show(&disp);
-        }
+        vTaskDelay(pdMS_TO_TICKS(60));
     }
 }
 
-void oled1_demo_2(void *p) {
+void echo_task(void *pvParameters) {
+    while (1) {
+        // Aguarda o início do eco
+        while (gpio_get(ECHO_PIN) == 0) {
+            if (alarm_fired) {
+                xSemaphoreGive(xSemaphoreTimeDiff);
+            }
+        }
+        time_start = time_us_32(); // Atualiza o tempo de início quando o eco começa
+
+        while (gpio_get(ECHO_PIN) == 1) {
+            if (alarm_fired) {
+                xSemaphoreGive(xSemaphoreTimeDiff);
+            }
+        }
+
+        time_end = time_us_32();
+
+        // Calcula a distância com base no tempo do eco
+        time_diff = time_end - time_start;
+        xSemaphoreGive(xSemaphoreTimeDiff);
+    }
+}
+
+void oled_task(void *pvParameters) {
     printf("Inicializando Driver\n");
     ssd1306_init();
 
@@ -105,32 +129,67 @@ void oled1_demo_2(void *p) {
     printf("Inicializando btn and LEDs\n");
     oled1_btn_led_init();
 
+    // Inicializa o RTC e define a data e hora
+    datetime_t t = {
+        .year = 2024,
+        .month = 03,
+        .day = 04,
+        .dotw = 3, // 0 é domingo, então 3 é quarta-feira
+        .hour = 14,
+        .min = 50,
+        .sec = 00};
+    rtc_init();
+    rtc_set_datetime(&t);
+
     while (1) {
+        char str[20], time_str[20], progress_str[34];
+        xSemaphoreTake(xSemaphoreTimeDiff, portMAX_DELAY);
+        if (time_diff == -1) {
+            strcpy(str, "Distancia: ERRO");
+        } else {
+            int distance = time_diff / 58;
+            sprintf(str, "Distancia: %d cm", distance);
+
+            // Cria a barra de progresso
+            int progress = (distance > 30) ? 30 : distance;
+            memset(progress_str, '#', progress);
+            progress_str[progress] = '\0';
+        }
+
+        // Obtém a hora atual do RTC
+        rtc_get_datetime(&t);
+        sprintf(time_str, "%02d:%02d:%02d", t.hour, t.min, t.sec);
 
         gfx_clear_buffer(&disp);
-        gfx_draw_string(&disp, 0, 0, 1, "Mandioca");
+        gfx_draw_string(&disp, 0, 0, 1, str);
+        gfx_draw_string(&disp, 0, 10, 1, time_str);
+        gfx_draw_string(&disp, 0, 20, 1, progress_str);
+        printf("%s\n", str);
+        printf("%s\n", time_str);
+        printf("%s\n", progress_str);
+        vTaskDelay(pdMS_TO_TICKS(1000));
         gfx_show(&disp);
-        vTaskDelay(pdMS_TO_TICKS(150));
-
-        gfx_clear_buffer(&disp);
-        gfx_draw_string(&disp, 0, 0, 2, "Batata");
-        gfx_show(&disp);
-        vTaskDelay(pdMS_TO_TICKS(150));
-
-        gfx_clear_buffer(&disp);
-        gfx_draw_string(&disp, 0, 0, 4, "Inhame");
-        gfx_show(&disp);
-        vTaskDelay(pdMS_TO_TICKS(150));
     }
 }
 
 int main() {
     stdio_init_all();
+    gpio_init(TRIG_PIN);
+    gpio_set_dir(TRIG_PIN, GPIO_OUT);
+    gpio_init(ECHO_PIN);
+    gpio_set_dir(ECHO_PIN, GPIO_IN);
+    gpio_set_irq_enabled_with_callback(ECHO_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &pin_callback);
 
-    xTaskCreate(oled1_demo_2, "Demo 2", 4095, NULL, 1, NULL);
+    xSemaphore = xSemaphoreCreateBinary();
+    xSemaphoreTimeDiff = xSemaphoreCreateBinary();
+
+    xTaskCreate(trigger_task, "Trigger", 8190, NULL, 1, NULL);
+    xTaskCreate(echo_task, "Echo", 8190, NULL, 1, NULL);
+    xTaskCreate(oled_task, "OLED", 8190, NULL, 1, NULL);
 
     vTaskStartScheduler();
+    printf("Error al iniciar el scheduler\n");
 
-    while (true)
-        ;
+    while (1) {
+    }
 }
